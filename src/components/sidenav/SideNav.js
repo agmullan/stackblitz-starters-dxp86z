8aa2b019a5a7343a5862ef77edea8409eb17839d
agmullan <33397452+agmullan@users.noreply.github.com>
2023-08-15 15:29:14 -0400

    4 files modified

--- src/components/sidenav/SideNav.js
+++ src/components/sidenav/SideNav.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import '../../css/sidenav.css';
+import { Outlet, Routes } from 'react-router-dom';
 
 const SideNav = (props) => {
   return (
-    <div className="sidenav">
-      <div className="link selected">
-        <a href="">Clients</a>
+    <Routes>
+      <div className="sidenav">
+        <SideNavLink name="Dashboard" />
+        <SideNavLink name="Clients" />
+        <SideNavLink name="Appointments" />
+        <SideNavLink name="Inventory" />
       </div>
-      <div className="link">
-        <a href="">Appointments</a>
-      </div>
-      <div className="link">
-        <a href="">Inventory</a>
-      </div>
-    </div>
+    </Routes>
   );
 };
 export default SideNav;
+
+const SideNavLink = (props) => {
+  return (
+    <div className="link">
+      <a href="">{props.name}</a>
+    </div>
+  );
+};
